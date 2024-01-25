9166015c69c9c5a8f01f6225ff89eaec67e5066b
Shebin John <admin@remedcu.com>
2024-01-25 16:21:55 +0530

    Using deployment.fixture

--- test/SafeTokenLock.spec.ts
+++ test/SafeTokenLock.spec.ts
@@ -1,20 +1,17 @@
-import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers'
 import { expect } from 'chai'
-import { ethers } from 'hardhat'
+import { deployments } from 'hardhat'
+import { getSafeTokenLock } from './utils/setup'
 
 describe('Lock', function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployFixture() {
-    const SafeTokenLock = await ethers.getContractFactory('SafeTokenLock')
-    const safeTokenLock = await SafeTokenLock.deploy()
+  const setupTests = deployments.createFixture(async ({ deployments }) => {
+    await deployments.fixture()
+    const safeTokenLock = await getSafeTokenLock()
     return { safeTokenLock }
-  }
+  })
 
   describe('Deployment', function () {
     it('Should deploy', async function () {
-      const { safeTokenLock } = await loadFixture(deployFixture)
+      const { safeTokenLock } = await setupTests()
 
       expect(safeTokenLock).to.exist
     })
