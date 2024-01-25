952e711c85f5ad2a8764d4ccb6fb2f19dc32bc5b
Shebin John <admin@remedcu.com>
2024-01-25 19:44:12 +0530

    Revert "Adding Safe Token dependency and deployment scripts"
    This reverts commit 4f85051d51e35497574f72ed7f0233f294517fba.

--- test/SafeTokenLock.spec.ts
+++ test/SafeTokenLock.spec.ts
@@ -1,22 +1,19 @@
 import { expect } from 'chai'
 import { deployments } from 'hardhat'
-import { getSafeTokenLock, getSafeToken } from './utils/setup'
+import { getSafeTokenLock } from './utils/setup'
 
 describe('Lock', function () {
   const setupTests = deployments.createFixture(async ({ deployments }) => {
     await deployments.fixture()
-
     const safeTokenLock = await getSafeTokenLock()
-    const safeToken = await getSafeToken()
-    return { safeTokenLock, safeToken }
+    return { safeTokenLock }
   })
 
   describe('Deployment', function () {
     it('Should deploy', async function () {
-      const { safeTokenLock, safeToken } = await setupTests()
+      const { safeTokenLock } = await setupTests()
 
       expect(safeTokenLock).to.exist
-      expect(await safeToken.decimals()).to.be.eq(18)
     })
   })
 })
--- test/utils/setup.ts
+++ test/utils/setup.ts
@@ -4,8 +4,3 @@ export const getSafeTokenLock = async () => {
   const SafeTokenLockDeployment = await deployments.get('SafeTokenLock')
   return await ethers.getContractAt('SafeTokenLock', SafeTokenLockDeployment.address)
 }
-
-export const getSafeToken = async () => {
-  const SafeTokenDeployment = await deployments.get('SafeToken')
-  return await ethers.getContractAt('SafeToken', SafeTokenDeployment.address)
-}
