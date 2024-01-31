7c03b3cf651a3c287f30a63f3a2a1cb5b6718651
Shebin John <admin@remedcu.com>
2024-01-31 15:44:08 +0530

    Update execution script

--- test/utils/execution.ts
+++ test/utils/execution.ts
@@ -1,5 +1,5 @@
 import { BigNumberish, Signer } from 'ethers'
-import { SafeToken } from '../../typechain-types/contracts/token/SafeToken.sol'
+import { SafeToken } from '../../typechain-types/safe-token/contracts/SafeToken'
 
 export const transferToken = (token: SafeToken, from: Signer, to: Signer, amount: BigNumberish) => {
   return token.connect(from).transfer(to, amount)
