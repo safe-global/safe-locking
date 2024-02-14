7850190e121e5517d5d44cb0c67b8c13c4212298
Nicholas Rodrigues Lordello <nick@safe.global>
2024-02-14 16:06:27 +0100

    Fix `yulcode` Task

--- src/tasks/show_codesize.ts
+++ src/tasks/show_codesize.ts
@@ -1,36 +1,39 @@
 import { task, types } from 'hardhat/config'
 import { loadSolc } from '../utils/solc'
 
-task('codesize', 'Displays the codesize of the contracts')
-  .addParam('skipcompile', 'should not compile before printing size', false, types.boolean, true)
-  .addParam('contractname', 'name of the contract', undefined, types.string, true)
-  .setAction(async (taskArgs, hre) => {
-    if (!taskArgs.skipcompile) {
+task('codesize', 'Displays the code size of the contracts')
+  .addParam('skipCompile', 'should not compile before printing size', false, types.boolean, true)
+  .addParam('contractName', 'name of the contract', undefined, types.string, true)
+  .setAction(async ({ skipCompile, contractName }, hre) => {
+    if (!skipCompile) {
       await hre.run('compile')
     }
     const contracts = await hre.artifacts.getAllFullyQualifiedNames()
     for (const contract of contracts) {
       const artifact = await hre.artifacts.readArtifact(contract)
-      if (taskArgs.contractname && taskArgs.contractname !== artifact.contractName) continue
+      if (contractName && contractName !== artifact.contractName) continue
       console.log(artifact.contractName, hre.ethers.dataLength(artifact.deployedBytecode), 'bytes (limit is 24576)')
     }
   })
 
-task('yulcode', 'Outputs yul code for contracts')
-  .addParam('contractname', 'name of the contract', undefined, types.string, true)
-  .setAction(async (taskArgs, hre) => {
+task('yulcode', 'Outputs Yul code for contracts')
+  .addParam('contractName', 'name of the contract', undefined, types.string, true)
+  .setAction(async ({ contractName }, hre) => {
     const contracts = await hre.artifacts.getAllFullyQualifiedNames()
     for (const contract of contracts) {
-      if (taskArgs.contractname && !contract.endsWith(taskArgs.contractname)) continue
+      if (contractName && !contract.endsWith(`:${contractName}`)) continue
       const buildInfo = await hre.artifacts.getBuildInfo(contract)
-      if (!buildInfo) return
-      console.log({ buildInfo })
+      if (!buildInfo) continue
       buildInfo.input.settings.outputSelection['*']['*'].push('ir', 'evm.assembly')
-      const solcjs = await loadSolc(buildInfo.solcLongVersion)
-      const compiled = solcjs.compile(JSON.stringify(buildInfo.input))
+      const solc = await loadSolc(buildInfo.solcLongVersion)
+      const compiled = solc.compile(JSON.stringify(buildInfo.input))
       const output = JSON.parse(compiled)
-      console.log(output.contracts[contract.split(':')[0]])
-      console.log(output.errors)
+      if (!output.errors) {
+        console.log(output.contracts[contract.split(':')[0]][contractName].ir)
+      } else {
+        console.error(output.errors)
+        process.exitCode = 1
+      }
     }
   })
 
