38148a3dc3a7e803cce660907f587d6c784229c5
Shebin John <admin@remedcu.com>
2024-02-15 15:28:19 +0530

    FORK_URL > HARDHAT_FORK_URL

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,8 @@ const argv = yargs(process.argv.slice(2))
 
 // Load environment variables.
 dotenv.config()
-const { CUSTOM_NODE_URL, FORK_URL, INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK, SOLIDITY_VERSION, SOLIDITY_SETTINGS, OWNER } = process.env
+const { CUSTOM_NODE_URL, HARDHAT_FORK_URL, INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK, SOLIDITY_VERSION, SOLIDITY_SETTINGS, OWNER } =
+  process.env
 
 const DEFAULT_MNEMONIC = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
 
@@ -53,7 +54,7 @@ const customNetwork = CUSTOM_NODE_URL
     }
   : {}
 
-const forking = FORK_URL ? { forking: { enabled: true, url: FORK_URL } } : {}
+const forking = HARDHAT_FORK_URL ? { forking: { enabled: true, url: HARDHAT_FORK_URL } } : {}
 
 const userConfig: HardhatUserConfig = {
   paths: {
