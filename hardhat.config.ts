c3bc509d25841f71394b8dda19fa413abd926731
Shebin John <admin@remedcu.com>
2024-02-15 18:42:06 +0530

    Update use of process.env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,8 @@ const argv = yargs(process.argv.slice(2))
 
 // Load environment variables.
 dotenv.config()
-const { CUSTOM_NODE_URL, INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK, SOLIDITY_VERSION, SOLIDITY_SETTINGS, OWNER } = process.env
+const { CUSTOM_NODE_URL, HARDHAT_FORK, INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK, SOLIDITY_VERSION, SOLIDITY_SETTINGS, OWNER } =
+  process.env
 
 const DEFAULT_MNEMONIC = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
 
@@ -25,7 +26,7 @@ if (PK) {
   }
 }
 
-if (['mainnet', 'sepolia'].includes(argv.network) && INFURA_KEY === undefined) {
+if ((['mainnet', 'sepolia'].includes(argv.network) || HARDHAT_FORK) && INFURA_KEY === undefined) {
   throw new Error(`Could not find Infura key in env, unable to connect to network ${argv.network}`)
 }
 
@@ -53,7 +54,7 @@ const customNetwork = CUSTOM_NODE_URL
     }
   : {}
 
-const forking = process.env['HARDHAT_FORK'] ? { forking: { enabled: true, url: `https://mainnet.infura.io/v3/${INFURA_KEY}` } } : {}
+const forking = HARDHAT_FORK ? { forking: { enabled: true, url: `https://mainnet.infura.io/v3/${INFURA_KEY}` } } : {}
 
 const userConfig: HardhatUserConfig = {
   paths: {
