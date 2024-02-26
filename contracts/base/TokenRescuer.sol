2226f29ac218889d2b3930b41adb30e3c4cd07e2
Nicholas Rodrigues Lordello <nick@safe.global>
2024-02-26 10:33:30 +0100

    Use Safe ERC20 Semantics For Token Rescue (#66)
    Fixes #60 
    
    This PR changes the Safe token rescuing functionality to use Safe ERC20
    transfer semantics. In particular, this allows non-standard ERC20
    implementations that don't return anything or return false instead of
    reverting to behave in a homogeneous way.
    
    Additionally, I moved the rescuing functionality into its own file, this
    keeps this "additional side logic" outside of the Safe token lock
    contracts (which IMO is cleaner). The function names were more closely
    changed to match the same contract in [the Safe token
    repository](https://github.com/safe-global/safe-token/blob/main/contracts/TokenRescuer.sol).
    Note that we keep the `amount` parameter in case some tokens don't allow
    transferring the full balance (although in practice this shouldn't
    matter much).
    
    I also chose to keep the behaviour where we don't revert when `amount ==
    0` in case there are some special semantics for tokens this is called
    on.
    
    Added a unit test.

---
+++ contracts/base/TokenRescuer.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity 0.8.23;
+
+import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/**
+ * @title TokenRescuer - Rescuing accidentally transferred ERC20 tokens.
+ * @author @safe-global/safe-protocol
+ * @custom:security-contact bounty@safe.global
+ */
+abstract contract TokenRescuer is Ownable2Step {
+    using SafeERC20 for IERC20;
+
+    /**
+     * @dev Hook that gets called before a rescuing a token.
+     * @param token Token that should be rescued
+     * @param beneficiary The account that should receive the tokens
+     * @param amount Amount of tokens that should be rescued
+     */
+    function _beforeTokenRescue(address token, address beneficiary, uint256 amount) internal virtual {}
+
+    /**
+     * @notice Rescues the specified `amount` of `tokens` to `beneficiary`. Can only be called by the {owner}.
+     * @param token Token that should be rescued
+     * @param beneficiary The account that should receive the tokens
+     * @param amount Amount of tokens that should be rescued
+     */
+    function rescueToken(address token, address beneficiary, uint256 amount) external onlyOwner {
+        _beforeTokenRescue(token, beneficiary, amount);
+        IERC20(token).safeTransfer(beneficiary, amount);
+    }
+}
---
+++ contracts/test/TestNonStandardERC20.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.8.0 <0.9.0;
+
+contract TestNonStandardERC20 {
+    enum Behaviour {
+        RETURN_FALSE_ON_FAILURE,
+        RETURN_NOTHING_ON_SUCCESS
+    }
+
+    Behaviour public immutable BEHAVIOUR;
+
+    constructor(Behaviour behaviour) {
+        BEHAVIOUR = behaviour;
+    }
+
+    function transfer(address, uint256) external view {
+        if (BEHAVIOUR == Behaviour.RETURN_FALSE_ON_FAILURE) {
+            // solhint-disable-next-line no-inline-assembly
+            assembly ("memory-safe") {
+                mstore(0, 0)
+                return(0, 32)
+            }
+        } else if (BEHAVIOUR == Behaviour.RETURN_NOTHING_ON_SUCCESS) {
+            return;
+        }
+    }
+}
