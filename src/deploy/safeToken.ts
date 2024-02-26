9f83bde94d9c203bbc40eb5a494789cf2e435cd5
Nicholas Rodrigues Lordello <nick@safe.global>
2024-02-26 10:15:26 +0100

    Parameterize All Deployment Parameters (#70)
    This PR changes the deployment process a bit such that all constructor
    arguments are parameterized.
    
    I also noticed that the test specs assume that `SafeToken.owner ==
    SafeTokenLock.owner` which isn't necessarily true, so I fixed that (in
    order for full parameterization to work :smile:).
    
    Instead of using named accounts, I opted for a `deployments` module with
    a `getDeploymentParameters` function, as not all arguments are supported
    this way, and this keeps all the logic for determining which constructor
    arguments to use in a single file.

--- src/deploy/safeToken.ts
+++ src/deploy/safeToken.ts
@@ -1,15 +1,17 @@
 import { network } from 'hardhat'
 import { DeployFunction } from 'hardhat-deploy/types'
+import { getDeploymentParameters } from '../utils/deployment'
 import { isForkedNetwork } from '../utils/e2e'
 
 const deploy: DeployFunction = async ({ deployments, getNamedAccounts }) => {
-  const { deployer, owner } = await getNamedAccounts()
+  const { deployer } = await getNamedAccounts()
   const { deploy } = deployments
 
+  const { initialOwner } = getDeploymentParameters()
   if (network.name == 'hardhat' && !isForkedNetwork()) {
     await deploy('SafeToken', {
       from: deployer,
-      args: [owner],
+      args: [initialOwner],
       log: true,
       deterministicDeployment: true,
     })
--- test/utils/setup.ts
+++ test/utils/setup.ts
@@ -1,8 +1,4 @@
 import { deployments, ethers } from 'hardhat'
-import { isForkedNetwork } from '../../src/utils/e2e'
-import { Address } from 'hardhat-deploy/types'
-
-export const cooldownPeriod = 60n * 60n * 24n * 30n // 30 days
 
 export const getSafeTokenLock = async () => {
   const SafeTokenLockDeployment = await deployments.get('SafeTokenLock')
@@ -10,12 +6,6 @@ export const getSafeTokenLock = async () => {
 }
 
 export const getSafeToken = async () => {
-  let SafeTokenDeploymentAddress
-  if (isForkedNetwork()) {
-    const { SAFE_TOKEN } = process.env
-    SafeTokenDeploymentAddress = SAFE_TOKEN as Address
-  } else {
-    SafeTokenDeploymentAddress = (await deployments.get('SafeToken')).address
-  }
-  return await ethers.getContractAt('SafeToken', SafeTokenDeploymentAddress)
+  const safeTokenLock = await getSafeTokenLock()
+  return await ethers.getContractAt('SafeToken', await safeTokenLock.SAFE_TOKEN())
 }
