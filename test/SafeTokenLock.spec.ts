043568411e770ae861f3c61d6d04b4c23c9266e1
Nicholas Rodrigues Lordello <nick@safe.global>
2024-02-27 10:56:17 +0100

    comments

--- test/SafeTokenLock.spec.ts
+++ test/SafeTokenLock.spec.ts
@@ -392,7 +392,7 @@ describe('SafeTokenLock', function () {
       // Locking tokens of UnlockN contract
       await unlockN.lockAll()
 
-      // Multiple unlocks in a single transaction do not revert, up to a maximum of 1154 from the block gas limit.
+      // Multiple unlocks in a single transaction do not revert, up to a maximum of 1167 from the block gas limit.
       await expect(unlockN.unlock(1167, { gasLimit: 30e6 })).to.not.be.rejected
       await expect(unlockN.unlock(1168, { gasLimit: 30e6 })).to.be.rejected
     })
